f5cfb4e1b71599e44f62de7db6fee6a7fa081fec
4K05H <rbenakos@gmail.com>
2023-03-18 10:31:58 +0100

    html be kiiras

--- obj.js
+++ obj.js
@@ -1,8 +1,8 @@
 window.addEventListener("load", init);
 
-function init(){
+const KUTYAK =[
 
-    const KOLOMPAR = {
+    {
 
         nev:"kolompár",
         fajta:"zsebrakéta",
@@ -12,24 +12,31 @@ function init(){
         kor:10,
         oltas:"van"
 
-    }
-    const ESZTEBAN = {
+    },
+    {
 
-        nev:"esztebán",
+        név:"esztebán",
         fajta:"sárkányrepülő",
         láb:"4",
-        marmagassag:30,
+        marmagasság:30,
         nem:"nem",
         kor:2,
-        oltas:"van"
+        oltás:"van"
 
     }
-    for (const key in KOLOMPAR) {
-        if (Object.hasOwnProperty.call(KOLOMPAR, key)) {
-            const element = KOLOMPAR[key];
-            console.log(element)
-            
+
+]
+
+function init(){
+
+    console.log(KUTYAK)
+    for (let index = 0; index < KUTYAK.length; index++) {
+        for (const kutyaTulaj in KUTYAK[index]) {
+           console.log(kutyaTulaj + ":", KUTYAK[index][kutyaTulaj])
         }
+        console.log("-------------*-------------")
+            
     }
+    
     
 }
--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 window.addEventListener("load", init);
 
-const kutyaLista=["foxterrier","kuvasz","puli","agár","újfullandi","keverék"]
+const KUTYAK =[
+
+    {
+
+        nev:"kolompár",
+        fajta:"zsebrakéta",
+        láb:"8",
+        marmagassag:69,
+        nem:"igen",
+        kor:10,
+        oltas:"van"
+
+    },
+    {
+
+        név:"esztebán",
+        fajta:"sárkányrepülő",
+        láb:"4",
+        marmagasság:30,
+        nem:"nem",
+        kor:2,
+        oltás:"van"
+
+    }
+
+]
 
 function init(){
 
     const ARTICLE = document.querySelectorAll("article");
     let htmlValtozo = kutyaKiir()
     ARTICLE[0].innerHTML = htmlValtozo
+    const GOMB = document.querySelector("button");
+    GOMB.addEventListener("click", )
 
 }
 
 function kutyaKiir(){
     let htmlValtozo = ""
-    for (let index = 0; index < kutyaLista.length; index++) {
-        htmlValtozo += `<div><p> ${kutyaLista[index]} </p></div>`;
+    for (let index = 0; index < KUTYAK.length; index++) {
+        htmlValtozo += "<div>"
+        for (const kutyaTulaj in KUTYAK[index]) {
+            htmlValtozo += `<p> ${kutyaTulaj} : ${KUTYAK[index][kutyaTulaj]} <button>törlés</button></p>`;
+        }
+        htmlValtozo += "</div>"
+            
     }
-    console.log(htmlValtozo)
     return htmlValtozo;
-    
 }
